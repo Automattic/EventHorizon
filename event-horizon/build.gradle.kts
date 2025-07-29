3afb5ccaed26c7dfbbd55824d2909dfd943f12b4
Michał Sikora <me@mehow.io>
2025-07-29 22:14:51 +0200

    Remove unsued kaml dependency (#4)

--- event-horizon/build.gradle.kts
+++ event-horizon/build.gradle.kts
@@ -1,11 +1,9 @@
 plugins {
   alias(libs.plugins.kotlin.jvm)
-  alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
   api(libs.arrow.core)
-  implementation(libs.kaml)
 
   testImplementation(projects.eventHorizon.testing)
   testImplementation(libs.kotest.arrow)
