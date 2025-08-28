86729d4418fdf3183f23e4b0c6a5f0081ce5ac03
Michał Sikora <me@mehow.io>
2025-08-28 17:01:46 +0200

    Add Group type

---
+++ event-horizon/src/main/kotlin/com/automattic/eventhorizon/Group.kt
@@ -0,0 +1,41 @@
+package com.automattic.eventhorizon
+
+import arrow.core.Either
+import arrow.core.raise.Raise
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+
+@ConsistentCopyVisibility
+public data class Group private constructor(
+  val key: CaseString,
+  val name: String,
+  val description: String?,
+) {
+  public companion object {
+    public operator fun invoke(key: String, name: String?, description: String?): Either<GroupProblem, Group> = either {
+      val caseKey = ensureValidKey(key)
+      val name = name ?: caseKey.toHumanReadableString(uppercaseWords = false).replaceFirstChar(Char::uppercase)
+
+      Group(caseKey, name, description)
+    }
+  }
+}
+
+public sealed interface GroupProblem : Problem {
+  public data object BlankKey : GroupProblem {
+    override fun print(): String {
+      return "Group cannot have a blank key"
+    }
+  }
+
+  public data class UnknownKeyCase(val key: String) : GroupProblem {
+    override fun print(): String {
+      return "Group '$key' uses unsupported naming convention. ${Case.supportedConventionsMessage}"
+    }
+  }
+}
+
+private fun Raise<GroupProblem>.ensureValidKey(key: String): CaseString {
+  ensure(key.isNotBlank()) { GroupProblem.BlankKey }
+  return CaseString(key).mapLeft(GroupProblem::UnknownKeyCase).bind()
+}
